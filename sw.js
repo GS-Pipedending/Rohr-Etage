434d8407742dec82385d5288179f14d3a37b055b
GS-Pipedending <m3-jost@gmx.de>
2026-09-10 10:28:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
+const CACHE_NAME = 'rohr-etage-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
@@ -7,5 +13,7 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
